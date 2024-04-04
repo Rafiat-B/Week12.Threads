81e854bf6484649a02c375dbf6a039a49648b6f1
Rafiat <bolajirafiat@gmail.com>
2024-04-03 21:05:15 -0400

    Add project files.

---
+++ Week11.Threads/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Week11.Threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btn_Gen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(110, 99);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(449, 284);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btn_Gen
+            // 
+            this.btn_Gen.Location = new System.Drawing.Point(371, 27);
+            this.btn_Gen.Name = "btn_Gen";
+            this.btn_Gen.Size = new System.Drawing.Size(167, 41);
+            this.btn_Gen.TabIndex = 2;
+            this.btn_Gen.Text = "Generate";
+            this.btn_Gen.UseVisualStyleBackColor = true;
+            this.btn_Gen.Click += new System.EventHandler(this.btn_Gen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Gen);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btn_Gen;
+    }
+}
+
---
+++ Week11.Threads/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week11.Threads
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private static int Fibonacci(int n)
+        {
+            int a = 0, b = 1, c = 0;
+            if (n == 0)
+                return a;
+            for (int i = 2; i <= n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return b;
+        }
+
+        private List<double> GenFibonacci(double n)
+        {
+            List<double> fibonacci = new List<double>();
+            for (int i = 0; i < n; i++)
+            {
+                fibonacci.Add(Fibonacci(i));
+            }
+            return fibonacci;
+        }
+
+        private async void btn_Gen_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (!int.TryParse(textBox1.Text, out int n) || n <= 0)
+            {
+                MessageBox.Show("Enter a positive integer greater than zero");
+                textBox1.Text = " ";
+            }
+
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    int fibNumber = Fibonacci(i);
+                    BeginInvoke(new Action(() => listBox1.Items.Add(fibNumber)));
+                    Thread.Sleep(100);  
+                }
+                
+            });
+            textBox1.Text = " ";
+        }
+
+    }
+}
